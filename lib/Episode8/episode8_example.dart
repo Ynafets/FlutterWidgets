03539ad1f6cb5ec3eb570583d468397868a9a267
Ynafets <58225925+Ynafets@users.noreply.github.com>
2020-07-11 20:59:35 -0500

    Ejercicico8

--- lib/Episode8/episode8_example.dart
+++ lib/Episode8/episode8_example.dart
@@ -1,27 +1,39 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(Example8());
+
 class Example8 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold( //estructurar
-      appBar: AppBar( //Cuadro superior 
+    return Scaffold(
+      //estructurar
+      appBar: AppBar(
+        //Cuadro superior
         title: Text("AppBar"),
-      ), 
-      body: Column(  //lista widget en vertical
+      ),
+      body: Column(
+        //lista widget en vertical
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
-          Placeholder(), //widget de posición
-          //dibuja un recuadro que representa dónde se agregarán otros widgets 
-          Row(//lista widget en horizontal
+          Placeholder(
+              //fallbackWidth: 100 ,
+              //fallbackHeight: 100.0,
+              //color: Colors.red,
+              ), //widget de posición
+          //dibuja un recuadro que representa dónde se agregarán otros widgets
+          Row(
+            //lista widget en horizontal
             children: <Widget>[
-              Text("Widget 1"),   
-              Visibility(//widget de 
+              Text("Widget 1"),
+              Visibility(
+                //widget de visibilidad
                 child: Text("Widget 2"),
                 visible: false,
                 //replacement: Text("Widget r "),
               ),
-              Spacer(), //widget espaciador
+              Spacer(flex: 2), //widget espaciador
               Text("Widget 3"),
+              Spacer(flex: 1),
               Text("Widget 4"),
             ],
           ),
